d047269eb91e430c15f573ef9ef4acd17e4e1fdd
johnray-martinez <jrvmartinez@icloud.com>
2020-03-28 22:05:25 -0400

    finished styling loaded main page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef } from 'react';
-import './LandingPage.scss';
 import TraverseButton from '../../components/TraverseButton/TraverseButton';
+import './LandingPage.scss';
+import cloud from '../../assets/images/cloud.png';
+import bird from '../../assets/images/bird.gif';
+import rope from '../../assets/images/rope.png';
+
 
 export default function LandingPage(props){
   const landing = useRef();
@@ -12,9 +16,17 @@ export default function LandingPage(props){
       <TraverseButton text="CONTACT " target="/contact" position="bottom" targetDiv="#landing" history={props.history} />
       <TraverseButton text="PROJECTS" target="/projects" position="right" targetDiv="#landing" history={props.history} />
       <TraverseButton text="ABOUT ME" target="/about" position="left" targetDiv="#landing" history={props.history} />
+      <img className="landing-page__cloud" src={cloud} alt="cloud"/>
+      <img className="landing-page__cloud landing-page__cloud--right" src={cloud} alt="cloud"/>
+      <img className="landing-page__cloud--bottom" src={cloud} alt="cloud"/>
+      <img className="landing-page__cloud--bottom landing-page__cloud--right" src={cloud} alt="cloud"/>
       <div className="landing-page__title">
+        <img className="landing-page__bird--left" src={bird} alt="bird" />
+        <img className="landing-page__bird--right" src={bird} alt="bird" />
+        <img className="landing-page__rope--left" src={rope} alt="rope" />
+        <img className="landing-page__rope--right" src={rope} alt="rope" />
         <h1 className="landing-page__title--name">JOHN RAY MARTINEZ</h1>
-        <span>WEB DEVELOPER</span> <br/>
+        <span className="landing-page__subtitle">W E B &nbsp; D E V E L O P E R </span> <br/>
       </div>
     </section>
   );
