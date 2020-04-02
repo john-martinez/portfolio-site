67d62f39c37d3256f50a69c27cb174d48abc4215
John Ray Martinez <jrvmartinez@icloud.com>
2020-04-02 01:01:58 -0400

    finished cards for projects

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -5,8 +5,8 @@ import pic from '../../assets/images/fatbird.png';
 import './Project.scss';
 
 export default function Project({project}){
-  const linkHandler = () => window.open(project.link, '_blank')
-  console.log(project)
+  const linkHandler = () => window.open(project.link, '_blank');
+  const demoHandler = () => window.open(project.demoLink, '_blank');
   return(
     <div id="project" className="project" >
       <div className="project__left" onClick={linkHandler} >
@@ -17,18 +17,21 @@ export default function Project({project}){
         showIndicators={false} 
         interval={5000}
         showArrows={false}
+        swipeable={false}
         >
-          {project.photos.map(item=>(
-            <div className="project__carousel-item">
-            <img className="project__carousel-image" src={item} />
-          </div>
+          {project.photos.map((item,i)=>(
+            <div key={i} className="project__carousel-item">
+              <img className="project__carousel-image" src={item} />
+            </div>
           ))}
         </Carousel>
       </div>
       <div className="project__right">
-        <strong className="project__title">{project.name}</strong> 
-        {project.tech.map(item=><span className="project__tech">{item}</span>)} <br/>
-        <span>{project.description}</span> <br/>
+        <strong className="project__title">{project.name}</strong> <br/>
+        <span className="project__blurb">{project.description}</span> <br/>
+        {project.tech.map((item,i)=><span key={i} className="project__tech">{item}</span>)} <br/>
+        <button onClick={linkHandler}>Site</button>
+          {project.demoLink ? <button onClick={demoHandler}>Watch Demo</button> : '' }
       </div>    
     </div>
   );
--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -8,6 +8,9 @@ import suber3 from '../../assets/images/suber3.png';
 import suber4 from '../../assets/images/suber4.png';
 import suber5 from '../../assets/images/suber5.png';
 import suber6 from '../../assets/images/suber6.png';
+import bttw1 from '../../assets/images/bttw1.png';
+import bttw2 from '../../assets/images/bttw2.png';
+import bttw3 from '../../assets/images/bttw3.png';
 import './ProjectsList.scss';
 
 export default function ProjectsList(){
@@ -21,7 +24,19 @@ export default function ProjectsList(){
         <FontAwesomeIcon icon={faSass}/>
       ],
       link: "https://subergame.netlify.com/",
-      photos: [suber1,suber2,suber3,suber4,suber5,suber6]
+      photos: [suber1,suber2,suber3,suber4,suber5,suber6],
+      demoLink: "https://www.loom.com/share/c3b15d35e7874d2e8ec41575a4e7a229"
+    },
+    {
+      name: "BetterThanTheWeather",
+      description: "A dashboard app that shows your local time and changes the background depending on the weather and time in your area",
+      tech: [
+        <FontAwesomeIcon icon={faNode}/>, 
+        <FontAwesomeIcon icon={faReact}/>, 
+        <FontAwesomeIcon icon={faSass}/>
+      ],
+      link: "https://bttweather.netlify.com/",
+      photos: [bttw1,bttw2,bttw3]
     }
   ]
   return(
