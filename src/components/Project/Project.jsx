cc9505efb0b195be0b9484b002d063199848cb8d
John Ray Martinez <jrvmartinez@icloud.com>
2020-04-01 20:40:35 -0400

    created cards for projects

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import Modal from 'react-modal';
+import pic from '../../assets/images/fatbird.png';
 import './Project.scss';
 
-export default function Project(){
+export default function Project({project}){
+  const linkHandler = () => window.open(project.link, '_blank')
+  console.log(project)
   return(
-    <div className="project">
-        <div className="project__left">
-          <div className="project__img"></div>
-        </div>
-        <div className="project__right">
-          <p>Project Title: Suber</p>
-          <p>Description: blurb</p>
-        </div>
+    <div id="project" className="project" >
+      <div className="project__left" onClick={linkHandler} >
+        <Carousel 
+        showThumbs={false} 
+        autoPlay={true} 
+        infiniteLoop={true} 
+        showIndicators={false} 
+        interval={5000}
+        showArrows={false}
+        >
+          {project.photos.map(item=>(
+            <div className="project__carousel-item">
+            <img className="project__carousel-image" src={item} />
+          </div>
+          ))}
+        </Carousel>
       </div>
+      <div className="project__right">
+        <strong className="project__title">{project.name}</strong> 
+        {project.tech.map(item=><span className="project__tech">{item}</span>)} <br/>
+        <span>{project.description}</span> <br/>
+      </div>    
+    </div>
   );
 }
--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import Project from '../Project/Project';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faNode, faReact, faSass } from '@fortawesome/free-brands-svg-icons';
+import suber1 from '../../assets/images/suber1.png';
+import suber2 from '../../assets/images/suber2.png';
+import suber3 from '../../assets/images/suber3.png';
+import suber4 from '../../assets/images/suber4.png';
+import suber5 from '../../assets/images/suber5.png';
+import suber6 from '../../assets/images/suber6.png';
 import './ProjectsList.scss';
 
 export default function ProjectsList(){
+  let projects = [
+    {
+      name: "Süber",
+      description: "An arcade driving game that punishes players for falling asleep or getting drunk in game",
+      tech: [
+        <FontAwesomeIcon icon={faNode}/>, 
+        <FontAwesomeIcon icon={faReact}/>, 
+        <FontAwesomeIcon icon={faSass}/>
+      ],
+      link: "https://subergame.netlify.com/",
+      photos: [suber1,suber2,suber3,suber4,suber5,suber6]
+    }
+  ]
   return(
     <div className="projects-list">
-      <Project />
-      <Project />
+      {projects.map((project,i)=><Project key={i} project={project}/>)}
     </div>
   );
 }
