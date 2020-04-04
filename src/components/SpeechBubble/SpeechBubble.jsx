7904394cdbb5f17d77ad8af4e7be204a185aa2b8
johnray-martinez <jrvmartinez@icloud.com>
2020-04-04 00:00:42 -0400

    finished styling mobile for about me

--- src/components/SpeechBubble/SpeechBubble.jsx
+++ src/components/SpeechBubble/SpeechBubble.jsx
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import './SpeechBubble.scss';
 
 export default function SpeechBubble({speech, handler}){
@@ -25,7 +27,7 @@ export default function SpeechBubble({speech, handler}){
     return(
       <div className="speech-bubble">
         <div className="speech-bubble__blurb">
-          <h4>Hello Visitor!</h4>
+          <h4 className="speech-bubble__blurb-headline">Hello Visitor!</h4>
           <span>Are you interested at what people are saying about John Ray?</span>
           <button className="speech-bubble__button" onClick={handler}>Yes</button>
         </div>
@@ -37,13 +39,22 @@ export default function SpeechBubble({speech, handler}){
     return(
       <div className="speech-bubble">
         <div className="speech-bubble__blurb">
-          <span>{`${speech[line].author} ${speech[line].company ? ' from ' + speech[line].company : ''} said: `}</span>
-          <h5>{`${speech[line].headline}`}</h5>
+          <h5 className="speech-bubble__blurb-headline">{`${speech[line].headline}`}</h5>
           <p className="speech-bubble__blurb-message">
             {`"${speech[line].message}"`}
           </p>
-          <button onClick={getPreviousMessage}>prev</button>
-          <button onClick={getNextMessage}>next</button>
+          <p className="speech-bubble__author">
+            {`${speech[line].author}${speech[line].company ? `, ${speech[line].company}` : ''}`} <br/>
+            {` ${speech[line].position}`}
+          </p>
+          <div className="speech-bubble__button-container">
+            <span className="speech-bubble__arrow-button" onClick={getPreviousMessage}>
+              <FontAwesomeIcon icon={faArrowLeft} />  
+            </span>
+            <span className="speech-bubble__arrow-button" onClick={getNextMessage}>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </span>
+          </div>
         </div>
         <div className="speech-bubble__arrow"></div>
       </div>
